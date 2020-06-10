f33e3001526e326a6a7550bc3d91578abd3796dd
Seth <swidoff@gmail.com>
2020-06-10 09:06:43 -0700

    "Allocating Frames"

--- src/main.rs
+++ src/main.rs
@@ -34,17 +34,19 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
     rust_os::init();
 
-    let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let mut mapper = unsafe { memory::init(physical_memory_offset) };
-    let mut frame_allocator = memory::EmptyFrameAllocator;
+    // let physical_memory_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let mut mapper = unsafe { memory::init(physical_memory_offset) };
+    // let mut frame_allocator = unsafe {
+    //     memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
+    // };
 
     // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    // let page = Page::containing_address(VirtAddr::new(0));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
 
     // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
 
     #[cfg(test)] test_main();
 
--- src/memory.rs
+++ src/memory.rs
@@ -1,5 +1,6 @@
-use x86_64::{structures::paging::PageTable, VirtAddr, PhysAddr};
-use x86_64::structures::paging::{OffsetPageTable, Page, FrameAllocator, Size4KiB, PhysFrame, Mapper};
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
+use x86_64::{PhysAddr, structures::paging::PageTable, VirtAddr};
+use x86_64::structures::paging::{FrameAllocator, Mapper, OffsetPageTable, Page, PhysFrame, Size4KiB};
 
 /// Initialize a new OffsetPageTable
 ///
@@ -36,17 +37,46 @@ unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
     }
 }
 
-pub fn create_example_mapping(
-    page: Page,
-    mapper: &mut OffsetPageTable,
-    frame_allocator: &mut impl FrameAllocator<Size4KiB>) {
-    use x86_64::structures::paging::PageTableFlags as Flags;
+/// A FrameAllocator that returns usable frames from the bootloader's memory map.
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
 
-    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
-    let flags = Flags::PRESENT | Flags::WRITABLE;
+impl BootInfoFrameAllocator {
+    /// Create a FrameAllocator from the passed memory map.
+    ///
+    /// This function is unsafe because the caller must guarantee that the passed memory map is
+    /// valid. The main requirement is that all frames that are marked as `UNUSED` in it are
+    /// really unused.
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator { memory_map, next: 0 }
+    }
 
-    let map_to_result = unsafe {
-        mapper.map_to(page, frame, flags, frame_allocator)
-    };
-    map_to_result.expect("map_to failed").flush()
+    /// Returns an iterator over the usable frames specified in the memory map.
+    fn usable_frames(&self) -> impl Iterator<Item=PhysFrame> {
+        // Get usable regions from the memory map.
+        let regions = self.memory_map.iter();
+        let usable_regions =
+            regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+
+        // Map each region to its address range.
+        let addr_ranges =
+            usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+
+        // Transform to an iterator of frame start addresses.
+        let frame_addresses =
+            addr_ranges.flat_map(|r| r.step_by(4096));
+
+        // Create `PhysFrame` types from the start addresses.
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame<Size4KiB>> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
 }
